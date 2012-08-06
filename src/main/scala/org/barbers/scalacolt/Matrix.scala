4c161c42b02d73efc32b5176ea74517e767fc1d8
Argyris <argyris@adgrok.com>
2012-08-05 19:10:08 -0700

    Add a few matrix methods and tests.

--- src/main/scala/org/barbers/scalacolt/Matrix.scala
+++ src/main/scala/org/barbers/scalacolt/Matrix.scala
@@ -2,14 +2,23 @@ package org.barbers.scalacolt
 
 import cern.colt.matrix.DoubleMatrix2D
 import cern.colt.matrix.impl.DenseDoubleMatrix2D
+import cern.colt.matrix.linalg.Algebra
 
 object Matrix {
   implicit def iterIterToMatrix(it : Iterable[Iterable[Double]]) : Matrix = {
     val ar = it.map{ _.toArray }.toArray
     new Matrix(new DenseDoubleMatrix2D(ar))
   }
+
+  private[scalacolt] def algebra = Algebra.DEFAULT
 }
 class Matrix(val cMatrix : DoubleMatrix2D) {
   def rows = cMatrix.rows
   def columns = cMatrix.columns
+  def toArray = cMatrix.toArray
+
+  // These are expensive so make them lazy
+  lazy val sum = cMatrix.zSum
+  lazy val trace = Matrix.algebra.trace(cMatrix)
+  lazy val rank = Matrix.algebra.rank(cMatrix)
 }
--- src/test/scala/org/barbers/scalacolt/MatrixSpec.scala
+++ src/test/scala/org/barbers/scalacolt/MatrixSpec.scala
@@ -3,12 +3,27 @@ package org.barbers.scalacolt
 import org.specs2.mutable._
 
 class MatrixSpec extends Specification {
-  "An iterable of iterables" should {
-    "implicitly convert to a Matrix" in {
-      import Matrix._
-      val a : Matrix = List(List(1.0,2.0,3.0),List(4.0,5.0,6.0))
+  import Matrix._
+  val a : Matrix = List(List(1.0,2.0,3.0),List(4.0,5.0,6.0))
+  val b : Matrix = List(List(1.0,2.0,3.0),List(4.0,5.0,6.0),List(7.0,8.0,9.0))
+
+  "A matrix object" should {
+    "have the right rows and columns" in {
       a.rows must_== 2
       a.columns must_== 3
+    }
+
+    "have the right trace" in {
+      a.trace must_== 6.0
+    }
+
+    "have the right sum" in {
+      a.sum must_== 21.0
+    }
+
+    // TODO: check why rank fails for rectangular matrices
+    "have the right rank" in {
+      b.rank must_== 2
     }
   }
 }
